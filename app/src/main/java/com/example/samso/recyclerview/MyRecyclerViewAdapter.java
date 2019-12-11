a72e0d0ca7c658c2524a0fec61d2f54cd4f9adb1
SamsonMaru <samsondemeke143@gmail.com>
2019-12-11 09:32:51 -0500

    dummy function added

--- app/src/main/java/com/example/samso/recyclerview/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/samso/recyclerview/MyRecyclerViewAdapter.java
@@ -64,5 +64,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
         }
 
+        //adding dummy function
+        void dummyFunction(){
+            System.out.print("I do not do anything. I am here just to help you learn how git merge works");
+        }
+
     }
 }
